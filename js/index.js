f36e02720ada1c3a9bde683c1d9c6902fee57145
Pavel <mpakunderscore@gmai.com>
2017-04-04 14:39:10 +0300

    Stable

--- js/index.js
+++ js/index.js
@@ -1,15 +1,39 @@
-function supportsImports() {
-    return 'import' in document.createElement('link');
-}
+$(document).ready(function() {
+
+    $("#container").load("./view/main.html");
+});
+
+// document.getElementById("body").requestFullscreen();
 
 function init() {
-    alert("init");
+
+    // document.body.requestFullscreen();
+
+    $("#container").load("./view/game.html", function () {
+
+        for (let i = 0; i < 10; i++) {
+
+            $("#field").append("<div class='rowCell' id='row" + i + "'></div>");
+
+            for (let j = 0; j < 10; j++) {
+
+                $("#row" + i).append("<div class='cell' id='cell" + i + j + "' onclick='cell(" + i + " ," + j + ")'></div>");
+            }
+        }
+
+        // window.scrollTo(0, 1);
+    });
 }
 
 function start() {
-    alert("start");
+    $("#container").load("./view/main.html" );
 }
 
 function random() {
-    alert("random");
+    console.log(Math.round(Math.random() * 10))
+}
+
+function cell(i, j) {
+    console.log("cell: " + i + " " + j);
+    $("#cell" + i + j).addClass("shot");
 }
