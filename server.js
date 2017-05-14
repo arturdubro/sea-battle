3657493e59f46f6b26a885540b7201d14ece6928
mpakunderscore <mpakunderscore@gmail.com>
2017-05-15 00:50:22 +0300

    Stable

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ let serveStatic = require('serve-static');
 
 const port = 4040;
 
-connect().use(serveStatic(__dirname)).listen(port, function(){
+connect().use('/', serveStatic(__dirname)).listen(port, function () {
     console.log('Server running on ' + port + '...');
 });
